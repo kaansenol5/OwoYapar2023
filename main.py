b15b66830e7da0d2cb5e062d9ae059c1c3d12502
kaan <moonman76@protonmail.com>
2019-03-28 21:51:52 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from praw.models import MoreComments
-import login,time
+import login,time,os
 reddit=login.reddit
-
+log=open("log.txt","w+")
 info="""
 \n
 \n
@@ -10,7 +10,7 @@ info="""
 ^(ben bot| [kod](https://www.github.com/kaansenol5/OwoYapar2023))
 """
 def reply(content,info):
-    new=content.replace("r","w").replace("l","w").replace("v","w")+ info
+    new=content.replace("r","w").replace("l","w").replace("v","w").replace("R","W").replace("L","W") + info
     return new
 
 subreddit=reddit.subreddit("kopyamakarna")
@@ -23,8 +23,18 @@ while True:
             if top_level_comment.author=="OwoYapar2023":
                 toReply=False
                 print("PASSED "+ submission.url)
+                log.write("PASSED "+ submission.url+"\n")
         if toReply:
-            new=reply(submission.selftext, info)
-            submission.reply(new)
+            try:
+                new=reply(submission.selftext, info)
+                submission.reply(new)
+            except Exception as e:
+                log.write("ERROR: " + str(e) + "   on submission " + submission.url)
             print("REPLIED "+ submission.url)
-            time.sleep(60)
+            log.write("REPLIED "+submission.url+"\n")
+            try:
+                time.sleep(60)
+            except KeyboardInterrupt:
+                log.close()
+                os.rename("log.txt","oldlog.txt")
+                exit()
