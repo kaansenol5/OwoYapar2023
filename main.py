5374124a707b5b075cc61e9dcc84c44ccd93aee3
kaan <kaansnl@icloud.com>
2021-01-18 00:03:56 +0300

    meta postların altına yorum yapma sorunu çözüldü

--- main.py
+++ main.py
@@ -33,27 +33,30 @@ subreddit=reddit.subreddit("kopyamakarna")
 
 while True:
     for submission in subreddit.new():
-        toReply=True
-        for top_level_comment in submission.comments:
-            if isinstance(top_level_comment, MoreComments):
-                continue
-            if not submission.is_self:
-                toReply=False
-            if top_level_comment.author=="OwoYapar2023" or submission.selftext=='':
-                toReply=False
-                print("PASSED "+ submission.url)
-                log.write("PASSED "+ submission.url+"\n")
-        if toReply:
-            try:
-                new=reply(submission.selftext, info)
-                submission.reply(new)
-            except Exception as e:
-                log.write("ERROR: " + str(e) + "   on submission " + submission.url)
-            print("REPLIED "+ submission.url)
-            log.write("REPLIED "+submission.url+"\n")
-            try:
-                time.sleep(60)
-            except KeyboardInterrupt:
-                log.close()
+        if submission.flair == "META":
+            pass
+        else:
+            toReply=True
+            for top_level_comment in submission.comments:
+                if isinstance(top_level_comment, MoreComments):
+                    continue
+                if not submission.is_self:
+                    toReply=False
+                if top_level_comment.author=="OwoYapar2023" or submission.selftext=='':
+                    toReply=False
+                    print("PASSED "+ submission.url)
+                    log.write("PASSED "+ submission.url+"\n")
+            if toReply:
+                try:
+                    new=reply(submission.selftext, info)
+                    submission.reply(new)
+                except Exception as e:
+                    log.write("ERROR: " + str(e) + "   on submission " + submission.url)
+                print("REPLIED "+ submission.url)
+                log.write("REPLIED "+submission.url+"\n")
+                try:
+                    time.sleep(60)
+                except KeyboardInterrupt:
+                    log.close()
 
-                exit()
+                    exit()
